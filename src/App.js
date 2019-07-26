3cbcf3c2cb82787f6eff298c9547b9c668bfad16
Nuntadet Tepsadtra [TH] <NTepsadtra@kerrylogistics.com>
2019-07-26 14:43:47 +0700

    Add location DDL component and load props location to DDL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LocationDropDown from './components/LocationDropDown';
 
 function App() {
+
+  let provinces = [
+    { name: 'Bangkok', id: 1 },
+    { name: 'Nonthaburi', id: 2 }
+  ]
+
   return (
     <div>
-      
+      <LocationDropDown locations={provinces} />
     </div>
   );
 }
